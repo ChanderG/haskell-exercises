c082a817b62cc244948f33b694c698491604c81e
ChanderG <mail@chandergovind.org>
2019-11-23 21:55:14 +0530

    add rpg weapon wear out problem

---
+++ rpg-weapon-wearout.hs
@@ -0,0 +1,6 @@
+-- The aim of this problem is as follows:
+
+-- 1. Begin with a simple rpg system, where you model a player with some items. The player uses the items to cause some effects. In the beginning, the items are all static, a sword does some damage.
+-- 2. Later the problem owner changes the problem slightly -> items can now wear out over use/time. How easy is it to update the code to deal with this change?
+
+-- In a OOP world, you would probably change the constant to a field in the Object, how would we do it here?
