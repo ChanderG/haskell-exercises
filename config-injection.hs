bfd1ea765b95a90bf5ddc55645169ed1c49c9ee3
ChanderG <mail@chandergovind.org>
2019-11-23 22:29:51 +0530

    add stub for trying out the reader monad

---
+++ config-injection.hs
@@ -0,0 +1,4 @@
+-- Understand Config injection using the Reader Monad.
+
+-- How do you pass global config/state information from top level to the level of your function call?
+-- The Reader Monad is meant to do this. Let's try it out with an actual example.
